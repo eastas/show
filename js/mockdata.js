143e63e064cd582fb341a5ecd552018b73d23458
eastas <139707139@qq.com>
2020-12-13 14:58:09 +0800

    1213

---
+++ js/mockdata.js
@@ -0,0 +1,178 @@
+let config = {
+    topNav: [{
+        name: "首页",
+        href: "#",
+    }, {
+        name: "分类",
+        href: "#",
+
+    }, {
+        name: "排行榜",
+        href: "#",
+
+    }, {
+        name: "世界",
+        href: "#",
+
+    }, {
+        name: "原创投稿",
+        href: "#",
+
+    }, {
+        name: "APP下载",
+        href: "#",
+
+    }, {
+        name: "IP合作",
+        href: "#",
+
+    }, {
+        name: "营销合作",
+        href: "#",
+
+    }, {
+        name: "条漫大赛",
+        href: "#",
+
+    }, ],
+    sideNav: [{
+
+            name: "个人中心",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "我的消息",
+            href: "#",
+            src: "",
+        }, {
+
+            name: "会员中心",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "浏览历史",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "我的关注",
+            href: "#",
+
+            iconsrc: "",
+        }, {
+
+            name: "创作中心",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "快看商城",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "游戏中心",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "意见反馈",
+            href: "#",
+            iconsrc: "",
+        }, {
+
+            name: "设置",
+            href: "#",
+            iconsrc: "",
+        },
+
+
+    ],
+    likeData: [{
+        imgsrc: "",
+        tag: ["剧情", "都市"],
+        name: "蝉女",
+        read: {
+            step: 52,
+            name: "留步",
+        },
+        update: {
+            step: 67,
+            name: "异性之间有纯友谊？",
+        }
+
+
+
+    }, {
+        imgsrc: "",
+        tag: ["剧情", "都市"],
+        name: "蝉女",
+        read: {
+            step: 52,
+            name: "留步",
+        },
+        update: {
+            step: 67,
+            name: "异性之间有纯友谊？",
+        }
+    }, ],
+    selfData: {
+        update: [
+
+            {
+                imgsrc: "",
+                tag: ["漫画"],
+                info: "集卡礼品收到了，啊啊啊，好开心 此处狂开心表情",
+                likeNum: 1,
+
+
+
+            }, {
+                imgsrc: "",
+                tag: ["剧情", "都市"],
+                name: "蝉女",
+                info: "哈哈，第一次发讨论，最主要的是我想把这几张图发一下，就发一下",
+                likeNum: 5,
+
+            },
+        ],
+        liked: [{
+                imgsrc: "",
+                tag: ["漫画"],
+                info: "集卡礼品收到了，啊啊啊，好开心 此处狂开心表情",
+                likeNum: 1,
+
+
+
+            }, {
+                imgsrc: "",
+                tag: ["剧情", "都市"],
+                name: "蝉女",
+                info: "哈哈，第一次发讨论，最主要的是我想把这几张图发一下，就发一下",
+                likeNum: 5,
+
+            },
+
+        ],
+        collect: [{
+                imgsrc: "",
+                tag: ["漫画"],
+                info: "集卡礼品收到了，啊啊啊，好开心 此处狂开心表情",
+                likeNum: 1,
+
+
+
+            }, {
+                imgsrc: "",
+                tag: ["剧情", "都市"],
+                name: "蝉女",
+                info: "哈哈，第一次发讨论，最主要的是我想把这几张图发一下，就发一下",
+                likeNum: 5,
+
+            },
+
+        ]
+    }
+}
